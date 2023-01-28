916157823cde4a771cf1360025ba93505075f312
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-01-28 16:46:26 +0900

    fix #4

---
+++ mysite/settings-local-sample.py
@@ -0,0 +1,4 @@
+# from django.core.management.utils import get_random_secret_key
+# get_random_secret_key()
+
+SECRET_KEY = ''
--- mysite/settings.py
+++ mysite/settings.py
@@ -1,10 +1,9 @@
 from pathlib import Path
+from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dp-ls@6tvqm0k5ihk-o_hnaad^te=_)2-2)=zlowjwfn_-=id4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
